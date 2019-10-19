3fd58a0ae9e1ad3755c942586b2aa2c73d9a8839
dnagafonov <denisagafonovwork@gmail.com>
2019-10-19 16:50:38 +0300

    fill actions/index.js, reducers/todos.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import {createStore, Provider} from 'react-redux'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
 
 const store = createStore();
 
